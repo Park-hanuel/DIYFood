9655801096c1b403178757e672c42266c330d208
Seowon Choi <chltjdnjs529@gmail.com>
2022-08-31 11:21:40 +0900

    식재료 시세 페이지 생성

--- frontend/src/views/5_mypage/MyPageView.vue
+++ frontend/src/views/5_mypage/MyPageView.vue
@@ -7,8 +7,8 @@
       <p style="font-size:1.5em">______님 환영합니다!</p>
     </div>
     <div style="display: inline-block;">
-      <a href="/user/myinfo/update">
-        <section id="content-box" style="margin-left:50px">
+      <a href="/user/myinfo/">
+        <section class="content-box">
           <img src="https://cdn-icons-png.flaticon.com/512/668/668709.png" alt="" class="img_style">
           <div style="margin-top:30px;">
             <h4 style="color:black">회원정보 수정</h4>
@@ -16,7 +16,7 @@
         </section>
       </a>
       <a href="">
-        <section id="content-box" style="margin-left:50px">
+        <section class="content-box">
           <img src="https://cdn-icons-png.flaticon.com/512/735/735742.png" alt="" class="img_style">
           <div style="margin-top:30px;">
             <h4 style="color:black">나의 식단 계획</h4>
@@ -24,7 +24,7 @@
         </section>
       </a>
       <a href="">
-        <section id="content-box" style="margin-left:50px">
+        <section class="content-box">
           <img src="https://cdn-icons-png.flaticon.com/512/2041/2041643.png" alt="" class="img_style">
           <div style="margin-top:30px;">
             <h4 style="color:black">나의 식단 분석</h4>
@@ -32,14 +32,12 @@
         </section>
       </a>
     </div>
-    <div style="text-align:center;" >
+    <div style="display: inline-block;">
       <a href="">
-        <input type="button" class="btn btn-lg next-button text-uppercase" value="로그아웃">
+        <input type="button" class="btn btn-lg next-button" value="로그아웃">
       </a>
-    </div>
-    <div style="text-align:center;" >
       <a href="">
-        <input type="button" class="btn btn-lg next-button text-uppercase" value="회원 탈퇴">
+        <input type="button" class="btn btn-lg next-button" value="회원 탈퇴">
       </a>
     </div>
   </body>
@@ -61,16 +59,16 @@ export default {
 </script>
 
 <style>
+@import "@/css/styles.css";
 body{background: #f3f3f3;}
-#content-box{
+.content-box{
   float: left;
   width: 300px;
   height: 400px;
   background-color:white;
-  margin-right: 10px;
-  margin-left: 10px;
-  margin-top: 10px;
-  margin-bottom: 0px;
+  margin: 30px;
+  margin-bottom: 10px;
+  margin-top:10px;
   border-radius: 20px;
   box-shadow: 5px 5px 5px 5px lightgray;
   padding-top: 30px;
@@ -87,8 +85,14 @@ body{background: #f3f3f3;}
   width: 150px;
   border-radius: 10px;
   box-shadow: 5px 5px 5px 5px lightgray;
-  margin: 30px;
-  border: none;
+  margin: 20px;
   background-color: #ffffff;
+  float: left;
+}
+.next-button:hover {
+  background-color: #f3f3f3;
+}
+.next-button:focus {
+  background-color: #f3f3f3;
 }
 </style>
