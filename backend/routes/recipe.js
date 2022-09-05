c8d5d3609e42aa035109a4a2d6ae8cb99f3b9bd0
ChaeLin <nias03@naver.com>
2022-09-05 19:15:42 +0900

    서버 스케쥴링 추가 및 일부 버그 수정

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -5,7 +5,35 @@ const models = require('../models');
 const axios = require('axios');
 const server = require('../server');
 const dotenv = require('dotenv');
+const { QueryTypes } = require('sequelize');
 dotenv.config();
+
+//레시피 리스트 조회 페이지 (진행중)
+router.get('/alllist', async function(req, res){
+    try{
+        const result = await getRecipeList(1,1000);
+        const result2 = await getRecipeList(1001,1500);
+        console.log( Object.keys(result).length);
+        console.log(result2);
+        console.log(Object.keys(result2).length)
+        Object.assign(result,result2);
+        const total = Object.keys(result).length;
+        console.log(total);
+    }catch(err){
+        console.error(err);
+    }
+})
+
+const getRecipeList = async function(startId, endId){
+    try{
+        const url = `https://openapi.foodsafetykorea.go.kr/api/${process.env.RECIPE_APIKEY}/COOKRCP01/json/${startId}/${endId}/`;
+        const response = await axios.get(url);
+        const itemData = response.data.COOKRCP01.row;
+        return itemData;
+    }catch(err){
+        console.log(err);
+    }
+}
 
 //각 레시피 상세 조회 페이지
 router.get('/list', async function(req, res){
@@ -25,9 +53,10 @@ router.get('/list', async function(req, res){
 //유저 선택 재료 기반 레시피 선정
 router.get('/userlist', async function(req, res, next){  
     try{
+        const date = req.query.date;
         //1.유저 재료 리스트 만들기
         const userId = res.locals.user.id;
-        // const userId = 4; 
+        // const userId = 7; 
         const userIngredientList  = [];
 
         // 기존 재료와 선택 재료 리스트를 합칩니다.
@@ -37,7 +66,8 @@ router.get('/userlist', async function(req, res, next){
         });
         const existList = await models.ExistIngredient.findAll({
             attributes : ['itemCode'],
-            where: { userId : userId },
+            where: { userId : userId,
+                date : date },
         });
 
         userList.forEach((item,idx)=>{
@@ -49,6 +79,9 @@ router.get('/userlist', async function(req, res, next){
 
         const userListSet = new Set(userIngredientList);
         const uniqueList = [...userListSet];
+        if(uniqueList.length < 4){
+            res.send('최소 4개 이상의 재료를 담아주세요!');
+        }
         uniqueList.sort();
 
         //2. 레시피 별 재료 갯수 확인
@@ -78,7 +111,7 @@ router.get('/userlist', async function(req, res, next){
         const recipeList =[];
 
         //foodCodeList.length 
-        for(let i = 0 ; i< 100  ; i++){
+        for(let i = 0 ; i< foodCodeList.length ; i++){
             const foodItemCode = await models.Recipe.findAll({
                 attributes: ['itemCode'],
                 where: {foodCode: foodCodeList[i]}
@@ -111,7 +144,9 @@ router.get('/userlist', async function(req, res, next){
             recipeResult = recipeList.sort(function(a,b){
                 return b.percent - a.percent;
             });
-
+            if(recipeResult == null){
+                res.send('일치하는 레시피가 없습니다!');
+            }
             res.send(recipeResult);
     }catch(err){
         console.error(err);
--- backend/server/index.js
+++ backend/server/index.js
@@ -4,18 +4,16 @@ const models = require('../models');
 dotenv.config();
 
 //axios using
-async function getTodayData(p_item_category_code, p_regday){
+async function getTodayData(p_item_category_code, p_regday) {
     try {
         let itemList = [];
         const p_cert_key = process.env.GROCERY_APIKEY;
-        const p_cert_id = 2676; 
+        const p_cert_id = 2676;
         const url = `http://www.kamis.or.kr/service/price/xml.do?action=dailyPriceByCategoryList&p_product_cls_code=01&p_regday=${p_regday}&p_convert_kg_yn=Y&p_item_category_code=${p_item_category_code}&p_cert_key=${p_cert_key}&p_cert_id=${p_cert_id}&p_returntype=json`;
         const response = await axios.get(url);
         const itemData = response.data.data.item;
-        console.log(itemData);
 
-
-        for(let i = 0 ; i< itemData.length; i++){
+        for (let i = 0; i < itemData.length; i++) {
             const itemName = itemData[i].item_name;
             const itemCode = itemData[i].item_code;
             const detailItemName = itemData[i].kind_name;
@@ -25,27 +23,26 @@ async function getTodayData(p_item_category_code, p_regday){
             const price = itemData[i].dpr2;
 
             let unit = itemData[i].unit;
-            if(unit.includes('g')){
+            if (unit.includes('g')) {
                 unit = '1kg';
             }
             const item = {
-                itemCode : itemCode,
-                detailItemCode : detailItemCode,
-                itemName : itemName,
-                detailItemName :detailItemName,
-                rank :rank,
-                price : price,
-                unit : unit,
+                itemCode: itemCode,
+                detailItemCode: detailItemCode,
+                itemName: itemName,
+                detailItemName: detailItemName,
+                rank: rank,
+                price: price,
+                unit: unit,
             }
             itemList.push(item);
             await models.LiveIngredient.create(item);
-
         }
-            //await liveIngredient.bulkCreate([itemList]);
+        //await liveIngredient.bulkCreate([itemList]);
 
-      } catch (error) {
+    } catch (error) {
         console.error(error);
-      }
+    }
 }
 
 module.exports = { getTodayData };
