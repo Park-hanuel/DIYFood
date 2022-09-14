cd31d73e6f24aaeb34821b7d78b6b3159f9e8bc5
rhymus314 <yuys03145@gmail.com>
2022-09-14 14:43:13 +0900

    식단분석

--- backend/routes/dietanalysis.js
+++ backend/routes/dietanalysis.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const UserInfoController = require("../controllers/dietanalysis");
 
 router.get("/analysis", UserInfoController.getUserInfo);
+router.put("/analysis", UserInfoController.setUserInfo);
 router.post("/analysis", UserInfoController.setUserInfo);
+
 router.get("/analysis/result", UserInfoController.getUserRecipeAnalysis);
 
 module.exports = router;
